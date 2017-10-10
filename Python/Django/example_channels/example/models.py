560289e779bbe8423c1aed14ae1d6202eae5ed23
Nathan Valmonte <valmonten@gmail.com>
2017-10-10 11:38:44 -0700

    Messed around with User model base

--- Python/Django/example_channels/example/models.py
+++ Python/Django/example_channels/example/models.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class LoggedInUser(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, related_name='logged_in_user')
+
+class Teacher(User):
+    pass
